d726a51f2500debe5b64d33efde7c12f406aab12
samehkamaleldin <sameh.kamaleldin@gmail.com>
2014-08-17 18:00:10 +0300

    added client module

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 var io     = require("socket.io");
-var client = require("socket.io-client")
 
 var server = io();
 
 server.on("connection", function (socket) {
+  
   var message = "Hello Idiot";
   socket.emit("message", message);
+
+  socket.on("talk", function (d) {
+    console.log("msg: "+ d);
+  });
+
 });
 
 server.listen(7171);
