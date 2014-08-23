b8db82682b7ce048b003e023409c73a12e5af8b4
YesSir <yaser.omar.abdelaziz@gmail.com>
2014-08-23 07:45:44 +0300

    Edit client UI

--- client.js
+++ client.js
@@ -1,14 +1,26 @@
 var io      = require("socket.io-client")
-
 var url     = "ws://localhost:7171";
 var options = { transports: ['websocket'] };
-
 var client  = io(url, options);
+var readlineSync = require("readline-sync");
+var stdin = process.openStdin();
+var Jetty = require("jetty");
 
+var jetty = new Jetty(process.stdout);
+jetty.clear();
 
+username = readlineSync.question("Enter a username : ");
 
-client.on("message", function (msg) {
-  console.log("msg: " + msg);
-  
-  client.emit("talk","ana ya wa7sh");
+client.on("server_on", function () {
+  jetty.moveTo([0,0]);
+  console.log("Now you can start chatting ..\n");
+  var stdin = process.openStdin();
+  stdin.on('data', function (chunk) {
+    chunk = chunk.slice(0, chunk.length - 1);
+    client.emit("talk", username, chunk);
+  });
+
+  client.on("toUsersTalk", function (sUsername, d) {
+    if(username != sUsername) jetty.text(sUsername + ": " + d + "\n");
+  });
 });
